ec2e57af9b20981daa834d425c953544cc42b7c4
Joshua Noakes <joshua.noakes1@icloud.com>
2023-12-20 18:33:29 +0000

    get products for base cards

---
+++ components/Cards/Product/index.vue
@@ -0,0 +1,39 @@
+<script lang="ts">
+import { useDark } from '@vueuse/core';
+const isDark = useDark();
+
+export default {
+    name: 'CardsProduct',
+    props: {
+        'name': {
+            type: String,
+            required: true
+        },
+        'description': {
+            type: String,
+            required: true
+        },
+        'speed': {
+            type: Array,
+            required: true
+        },
+        'details': {
+            type: Array,
+            required: true
+        }
+    },
+    setup() {
+        return {
+            isDark
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="card shadow" style="margin-bottom: 1rem;" :data-bs-theme="`${isDark ? 'light' : 'dark'}`">
+        <div class="card-body">
+            <h5 class="card-title">{{ description }} ({{ name }})</h5>
+        </div>
+    </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -10,8 +10,23 @@ useSeoMeta({
     twitterTitle: pageTitle,
     twitterDescription: pageDescription,
 });
+
+// get product data
+let { data: productData, error: productError } = await useFetch('/api/v1/getAllProducts', {
+    server: false
+});
 </script>
 
 <template>
-    <h1>Hello World :3</h1>
+    <h1>Products</h1>
+    <div class="row">
+        <div v-if="productError" class="col-12">
+            <div class="alert alert-danger" role="alert">
+                Failed to Get Products: {{ productError.message?.toString().trim() }}
+            </div>
+        </div>
+        <div v-else v-for="product in productData['data']" class="col-12 col-lg-3">
+            <CardsProduct :name="product['name']" :description="product['description']" :speed="product['speed']" :details="product['details']"/>
+        </div>
+    </div>
 </template>
